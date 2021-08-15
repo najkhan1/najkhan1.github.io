bf9c410554fc55a05967f3968c83ae7f008c7c0b
naj khan <najkhan184@gmail.com>
2021-08-15 01:28:46 +0100

    Deploy

---
+++ precache-manifest.5e83cf0f0b1bfb3e62349304db5cc011.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f9aba1a655e9cd1d77",
+    "url": "css/app.e968ef5e.css"
+  },
+  {
+    "revision": "88e95f4738b94668fccb23f989659523",
+    "url": "img/git.88e95f47.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a6f9c1247ea14f91895af30337462a40",
+    "url": "img/nk2.a6f9c124.png"
+  },
+  {
+    "revision": "04200574d6737ac0779b77499d55759c",
+    "url": "img/schoolApp.04200574.png"
+  },
+  {
+    "revision": "089d64fab29565519ce6eedd2b686c8a",
+    "url": "img/scraper.089d64fa.png"
+  },
+  {
+    "revision": "76a4d8a2c06838e6d445b8c683f4a1be",
+    "url": "img/weather.76a4d8a2.png"
+  },
+  {
+    "revision": "b5dd588c00755cfb2d622c0278b376f2",
+    "url": "img/web.b5dd588c.png"
+  },
+  {
+    "revision": "cd7f0cda59d44cf6187d8d688ef318f8",
+    "url": "index.html"
+  },
+  {
+    "revision": "e944f98acc8e1eddba9b",
+    "url": "js/about.27c9b61f.js"
+  },
+  {
+    "revision": "b4f9aba1a655e9cd1d77",
+    "url": "js/app.90c80d8e.js"
+  },
+  {
+    "revision": "1084992049460786b533",
+    "url": "js/chunk-vendors.391a6d16.js"
+  },
+  {
+    "revision": "e97c5160991cd3ef67cb4ebb1761dde9",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
